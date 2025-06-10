0b2b5e2ce56feb71446a286d8ed13efebc82957a
Demiux2 <greatprogram7@gmail.com>
2025-06-09 23:24:51 -0300

    Remaked debug messages, fixed bugs and maked the program less prone to errors

--- src/ge/load_map.c
+++ src/ge/load_map.c
@@ -15,17 +15,15 @@ char path2[125] = "./src/maps/";
 
 FILE *map_file;
 int walls[2048][4] = {0}, sectors[1024][1024] = {0};
-int w_mats[2048], w_mat = 0, sw_c = 0;
-float wall_colors[2048][3];
+int w_mats[2048] = {0}, w_mat = 0, sw_c = 0;
+float wall_colors[2048][3] = {0};
 
 int w_counter = 0, s_counter = 0;
 
 int map_loaded = 0;
 
-void open_map() {
-    if(map_loaded){
-        return;
-    }
+void open_map(){
+    if(map_loaded) return;
 
     strcpy(pip.map, pip.filename);
     strcat(path1, pip.map);
@@ -37,30 +35,27 @@ void open_map() {
     if(map_file == NULL){
         map_file = fopen(path2, "r");
         if(map_file == NULL){
-            printf("Error: could not open the map\n");
+            printf("Error: could not open the map.\n");
             exit(1);
         }
     }
 
     int line_counter = 1, header_counter = 0, w_quantity;
-    int i = 0, j = 0;
+    //int i = 0, j = 0;
     char line[MAX_LINE];
 
     srand(time(NULL));
 
-while(fgets(line, sizeof(line), map_file) != NULL){
+    while(fgets(line, sizeof(line), map_file) != NULL){
         line[strcspn(line, "\n")] = 0;
         line[strcspn(line, "\r")] = 0;
 
-        if(line[0] == '\0'){
-            continue; // línea vacía, la saltamos
-        }
+        if(line[0] == '\0') continue;
 
         if(line_counter == 1){
-            sscanf(line, "%s", pip.map_name);
             strcpy(pip.map_name, line);
             if(flags.dflag)
-		    printf("[DEBUG] Map name: %s\n", pip.map_name);
+		        printf("[DEBUG] Map name: %s\n", pip.map_name);
         }
 
         if(line_counter == 2){
@@ -73,95 +68,87 @@ while(fgets(line, sizeof(line), map_file) != NULL){
         }
 
         if(strcmp(line, "[WALLS]") == 0){
-	    if(flags.dflag)
-                printf("\n[DEBUG] Reading \"[WALLS]\":\n");
-
+	        if(flags.dflag)
+		        printf("\n[DEBUG] Reading [WALLS]:\n");
             header_counter = 1;
             continue;
         }
-        if(strcmp(line, "[SECTORS]") == 0){
-	if(flags.dflag)
-		printf("\n[DEBUG] Reading \"[SECTORS]\":\n");
 
+        if(strcmp(line, "[SECTORS]") == 0){
+	        if(flags.dflag)
+		        printf("\n[DEBUG] Reading [SECTORS]:\n");
             header_counter = 2;
             continue;
         }
 
         //Procesar [WALLS]
         if(header_counter == 1){
-
             if(sscanf(line, "%i %i %i %i %i", &walls[w_counter][0], &walls[w_counter][1], &walls[w_counter][2], &walls[w_counter][3], &w_mat) == 5){
                 wall_colors[w_counter][0] = ((float)rand() / (float)(RAND_MAX));
                 wall_colors[w_counter][1] = ((float)rand() / (float)(RAND_MAX));
                 wall_colors[w_counter][2] = ((float)rand() / (float)(RAND_MAX));
 
-		if(flags.dflag)
-                	printf("[DEBUG] Wall %d: p1(%d, %d), p2(%d, %d) - Color: (%f, %f, %f)\n", 
-                        	w_counter, walls[w_counter][0], walls[w_counter][1], 
-                        	walls[w_counter][2], walls[w_counter][3],
-                        	wall_colors[w_counter][0], wall_colors[w_counter][1], wall_colors[w_counter][2]);
-
+		    if(flags.dflag)
+                printf("[DEBUG] Wall %d: p1(%d, %d), p2(%d, %d) - Color: (%f, %f, %f)\n", 
+                    w_counter, walls[w_counter][0], walls[w_counter][1], 
+                    walls[w_counter][2], walls[w_counter][3],
+                    wall_colors[w_counter][0], wall_colors[w_counter][1], wall_colors[w_counter][2]);
                 w_counter++;
             }
-
-	    else{
-                printf("Error: Invalid wall format in line: %s\n", line);
-            }
+        else printf("Error: Invalid wall format in line: %s\n", line);
         }
 
         //Procesar [SECTORS]
         if(header_counter == 2){
             char *ptr = line;
-            int spc = 0;
-            if(sscanf(ptr, "%d", &sw_c) == 1){
+            int wall_count = 0;
 
-		if(flags.dflag)
-                	printf("[DEBUG] Number of walls in sector %d: %d\n", spc + 1, sw_c);
-                
-                while(*ptr != ' ' && *ptr != '\0'){
-                    ptr++;
-                }
-                if(*ptr == ' '){
-                    ptr++;
-                }
+            if(sscanf(ptr, "%d", &wall_count) == 1){
+                if(flags.dflag)
+                    printf("[DEBUG] Sector %d has %d walls, which IDs are: ", s_counter + 1, wall_count);
 
-                while(spc < sw_c && sscanf(ptr, "%d", &sectors[s_counter][spc]) == 1){
-		    if(flags.dflag)
-                    	printf("[DEBUG] Wall ID %d: %d\n", spc + 1, sectors[s_counter][spc]);
+                sectors[s_counter][0] = wall_count;
 
-                    while(*ptr != ' ' && *ptr != '\0'){
-                        ptr++;
+                while (*ptr != ' ' && *ptr != '\0') ptr++;
+                if (*ptr == ' ') ptr++;
+
+                for(int i = 1; i <= wall_count; i++){
+                    if(sscanf(ptr, "%d", &sectors[s_counter][i]) != 1){
+                        printf("Error: Missing wall ID in sector %d\n", s_counter + 1);
+                        break;
                     }
-                    if(*ptr == ' '){
-                        ptr++;
-                    }
-                    spc++;
+                    if(flags.dflag){
+                        printf("%d", sectors[s_counter][i]);
+                        if(i < wall_count)
+                            printf(", ");
+                        }
+
+                    while(*ptr != ' ' && *ptr != '\0') ptr++;
+                    if(*ptr == ' ') ptr++;
                 }
 
-                //Leer floor_height, floor_mat, ceiling_height y ceiling_mat
+                if(flags.dflag) printf("\n");
+
                 for(int i = 0; i < 4; i++){
-                    if(sscanf(ptr, "%d", &sectors[s_counter][sw_c + i]) == 1){
+                    if(sscanf(ptr, "%d", &sectors[s_counter][wall_count + 1 + i]) == 1){
                         ptr = strchr(ptr, ' ');
                         if(ptr) ptr++;
                     }
                     else{
+                        printf("Error: Invalid sector format in line: %s\n", line);
                         break;
                     }
                 }
-
                 s_counter++;
             }
-            else{
-                printf("Error: Could not get \"n_walls\" in the sector number %s\n", line);
-            }
+            else printf("Error: Could not read wall count in sector %d\n", s_counter - 1);   
         }
-
         line_counter++;
     }
+    fclose(map_file);
 }
 
 int render_map(){
-
     for(int i = 0; i < w_counter; i++){
         glColor3f(wall_colors[i][0], wall_colors[i][1], wall_colors[i][2]);
 
@@ -175,35 +162,27 @@ int render_map(){
     }
     
     for(int s = 0; s < s_counter; s++){
-        float floor_height = sectors[s][sw_c];
-        float ceiling_height = sectors[s][sw_c + 2];
+        int wall_count = sectors[s][0];
+        float floor_height = sectors[s][wall_count + 1];
+        float ceiling_height = sectors[s][wall_count + 3];
 
-        //Dibujar el piso
+        // Dibujar el piso
         glColor3f(0.243f, 0.259f, 0.294f); //"Anchor gray" para el piso
         glBegin(GL_POLYGON);
-        for(int i = 0; i < sw_c; i++){
-            int wall_id = sectors[s][i];  //ID de la pared
-            int x1 = walls[wall_id][0];
-            int y1 = walls[wall_id][1];
-
-            //Dibujar el piso (en y = floor_height)
-            glVertex3f(x1, floor_height, y1); 
+        for(int i = 1; i <= wall_count; i++){
+            int wall_id = sectors[s][i];
+            glVertex3f(walls[wall_id][0], floor_height, walls[wall_id][1]);
         }
         glEnd();
 
-        //Dibujar el techo
+        // Dibujar el techo
         glColor3f(0.607f, 0.607f, 0.560f); //"Trout gray" para el techo
         glBegin(GL_POLYGON);
-        for(int i = 0; i < sw_c; i++){
+        for(int i = 1; i <= wall_count; i++){
             int wall_id = sectors[s][i];
-            int x1 = walls[wall_id][0];
-            int y1 = walls[wall_id][1];
-
-            //Dibujar el techo (en y = ceiling_height)
-            glVertex3f(x1, ceiling_height, y1);
+            glVertex3f(walls[wall_id][0], ceiling_height, walls[wall_id][1]);
         }
         glEnd();
     }
-
     return 0;
 }
