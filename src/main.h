e0f1d36963867590c10428ae809f585dcbd24089
Demiux2 <greatprogram7@gmail.com>
2025-07-11 21:58:09 -0300

    Added player_config struct to main.h

--- src/main.h
+++ src/main.h
@@ -5,6 +5,13 @@ struct flagstruct{
     int fflag, dflag;
 };
 
+struct player_config{
+    char player_name[64], language_id[64];
+    int res_x, res_y;
+    int fullscreen;
+};
+
 struct flagstruct flags;
+struct player_config player_conf;
 
 #endif
